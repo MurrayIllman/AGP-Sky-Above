0754a401ce5cf60e35760becd321cb4d97e1109e
Murray <illman.murray@hotmail.com>
2017-10-10 18:07:48 +0100

    Actual code change
    Yes, for real this time.

--- Assets/Scripts/MouseAimCamera.cs
+++ Assets/Scripts/MouseAimCamera.cs
@@ -4,35 +4,20 @@ using System.Collections;
 public class MouseAimCamera : MonoBehaviour {
 	public GameObject target;
 	public float rotateSpeed = 5;
-	Vector3 offset;
+	public float distance = 10.0f;
+	public float pitch = 0.0f;
+	public float angle = 0.0f;
 
-	public Transform pivot;
-
-	void Start() {
-		offset = target.transform.position - transform.position;
-	}
-	
 	void LateUpdate() {
 		float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
 		float vertical = -Input.GetAxis("Mouse Y") * rotateSpeed;
 
-
-
-		target.transform.Rotate(0, horizontal, 0);
-		pivot.Rotate (vertical, 0, 0);
-
-		float desiredAngle = target.transform.eulerAngles.y;
-		float desiredLook = target.transform.eulerAngles.x;
-		Quaternion rotation = Quaternion.Euler(desiredLook, desiredAngle, 0);
-		transform.position = target.transform.position - (rotation * offset);
-		
-		transform.LookAt (target.transform);
-
 		float minRotation = -45;
 		float maxRotation = 45;
-		Vector3 currentRotation = transform.localRotation.eulerAngles;
-		currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation, maxRotation);
-		transform.localRotation = Quaternion.Euler (currentRotation);
+		angle += horizontal;
+		pitch = Mathf.Clamp (pitch + vertical, minRotation, maxRotation);
 
+		transform.position = target.transform.position - Quaternion.Euler(pitch, angle, 0.0f) * Vector3.forward * distance;
+		transform.LookAt (target.transform);
 	}
 }
