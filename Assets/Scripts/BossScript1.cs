b28bd2a423c7591e506dd6c6553de6102d1fba29
MurrayIllman <illman.murray@hotmail.com>
2017-10-13 21:01:37 +0100

    Minor changes, boss script began
    Imported over some older scritps from a previous project that relate.

---
+++ Assets/Scripts/BossScript1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossScript1 : MonoBehaviour {
+
+	public int HP = 500;
+	public Slider hpSlider;
+
+	void Start () 
+	{
+		hpSlider.value = HP;
+	}
+
+	void Update () 
+	{
+		
+	}
+}
