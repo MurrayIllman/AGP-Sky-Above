17ddf641571ea68aaf07890a96e9e7a489daf5d6
Murray <illman.murray@hotmail.com>
2017-11-23 13:18:30 +0000

    Tweeked stuff with the oceon

--- Assets/Scripts/patternTwo.cs
+++ Assets/Scripts/patternTwo.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class patternTwo : MonoBehaviour, IAttack {
 
 	public float rotationSpeed = 10;
+	public Material[] telegraphColours;
+	Renderer rend;
+
+	void Start() {
+		rend = GetComponentInChildren<Renderer> ();
+		rend.enabled = true;
+		rend.sharedMaterial = telegraphColours[0];
+	}
 
 	public IEnumerator Attack () {
 		float rotation = 0.0f;
+		rend.sharedMaterial = telegraphColours [1];
+		yield return new WaitForSeconds (0.5f);
 
-		while (rotation < 360.0f) {
-
+		while (rotation < 180.0f) {
+			rend.sharedMaterial = telegraphColours [1];
 			rotation += rotationSpeed * Time.deltaTime;
 			transform.localRotation = Quaternion.Euler (0.0f, -rotation, 0.0f);
 			yield return null;
+			rend.sharedMaterial = telegraphColours [0];
 		}
 
 		transform.localRotation = Quaternion.Euler (0.0f, 0.0f, 0.0f);
